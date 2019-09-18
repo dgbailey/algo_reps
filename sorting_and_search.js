284c744f1ef7311d84adb22c272b8d4153e30ebb
dgbailey <bailey.dust@gmail.com>
2019-09-17 18:43:19 -0700

    tried to add linked list to help tc did not work for notification problem

--- sorting_and_search.js
+++ sorting_and_search.js
@@ -61,3 +61,96 @@ function activityNotifications(expenditure, d) {
     }
 
     return notificationCount}
+
+    //writing my own linked list did not improve time of above
+    class LinkedList{
+        constructor(node){
+            this.head = node
+            this.tail = null
+        }
+        removeHead(){
+            this.head = this.head.next
+           
+        }
+        addToTail(node){
+            if(this.head.next === null){
+                this.head.next = node
+                this.tail = node
+            }
+            if(this.tail){
+                this.tail.next = node
+                this.tail = node
+            }
+            else{
+                this.tail = node
+            }
+    
+        }
+    }
+    
+    class LlNode{
+        constructor(next = null,data){
+            this.next = next
+            this.data = data
+        }
+    }
+    // Complete the activityNotifications function below.
+    function activityNotifications(expenditure, d) {
+        
+        function median(ll){
+            let temp = []
+            let current = ll.head
+            while(current){
+                temp.push(current.data)
+                current = current.next
+            }
+    
+            temp.sort((a,b) => a-b)
+            if(temp.length % 2 === 0){
+                let med = (temp[temp.length/2 -1] + temp[temp.length/2])/2
+                return med
+            }
+            return temp[Math.floor(temp.length/2)]
+        }
+    
+        //edge case
+        if(expenditure.length <= d){
+            return 0
+        }
+    
+        //start main function
+        let notificationCount = 0
+        let progress = 1
+        let start = new LlNode(null,expenditure[0])
+        let tempArr = new LinkedList(start)
+        
+        
+        for(let i = 1; i < expenditure.length; i++){
+            if( progress >= d){
+                //O nlog(n)
+                let med = median(tempArr)
+               
+                if(2*med <= expenditure[i]){
+                    notificationCount++
+                }
+                //O(n)
+                tempArr.removeHead()
+                
+               
+                //compute median
+                //compare to d 
+                //increment notifications count or not
+                //clear temp arr
+            }
+            let newNode = new LlNode(null,expenditure[i])
+            tempArr.addToTail(newNode)
+            progress++
+        }
+    
+        return notificationCount
+    
+    
+        
+    
+    
+    }
