3573d405b08bacffae1a0f7b8f8f2f491632ba42
dgbailey <bailey.dust@gmail.com>
2019-09-17 18:06:43 -0700

    nofitication exercise not running with appropriate time complexity

--- sorting_and_search.js
+++ sorting_and_search.js
@@ -18,3 +18,46 @@ function maximumToys(prices, k) {
         }
     }
     return totalGifts}
+
+    //activitiy notifications
+
+function activityNotifications(expenditure, d) {
+    
+    function median(arr){
+        arr.sort((a,b) => a-b)
+        if(arr.length % 2 === 0){
+            let med = (arr[arr.length/2 -1] + arr[arr.length/2])/2
+            return med
+        }
+        return arr[Math.floor(arr.length/2)]
+    }
+
+    //edge case
+    if(expenditure.length <= d){
+        return 0
+    }
+
+    //start main function
+    let notificationCount = 0
+    let progress = 0
+    let tempArr = []
+    for(let i = 0; i < expenditure.length; i++){
+        if( progress >= d){
+            let med = median(tempArr)
+           
+            if(2*med <= expenditure[i]){
+                notificationCount++
+            }
+            tempArr.shift()
+            
+           
+            //compute median
+            //compare to d +1
+            //increment notifications count or not
+            //clear temp arr
+        }
+        tempArr.push(expenditure[i])
+        progress++
+    }
+
+    return notificationCount}
