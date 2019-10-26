2c9d93c8819b7186a1ec71d3c50062fe4f01ea1c
dgbailey <bailey.dust@gmail.com>
2019-10-26 14:32:09 -0700

    max heapify added

--- jsHeap.js
+++ jsHeap.js
@@ -1,9 +1,20 @@
 
+// The depth of a node is the number of edges from the node to the tree's root node.
+// A root node will have a depth of 0.
+
+// The height of a node is the number of edges on the longest path from the node to a leaf.
+// A leaf node will have a height of 0.
+
+
 class minHeap{
 
     constructor(){
         this.queue = []
 
+    }
+
+    getMax(){
+        return this.queue[0];
     }
 
     calcParent(index){
@@ -33,22 +44,129 @@ class minHeap{
 
     bubbleUp(queue, index){
 
+        
         let parent = this.calcParent(index);
         let child = index;
 
-        if(this.queue[child] < this.queue[parent]){
+        if(this.queue[child]){
+            if(this.queue[child] > this.queue[parent]){
 
-            let temp = this.queue[parent]
-            this.queue[parent] = this.queue[child];
-            this.queue[child] = temp;
+                let temp = this.queue[parent]
+                this.queue[parent] = this.queue[child];
+                this.queue[child] = temp;
+    
+                this.bubbleUp(this.queue, parent);
+            }
 
-            this.bubbleUp(this.queue, parent);
         }
+       
 
        
 
     }
-    //trickle down
+
+    
+
+    trickleDown(queue,index,end){
+        //max heap
+        let parent = index;
+        let firstChild = 2*index + 1;
+        let secondChild = 2*index+2;
+        let temp = queue[parent];
+        
+
+
+        
+        if(queue[firstChild]){
+           
+            if(end){
+
+                if(firstChild === end || secondChild === end){
+
+                    return
+                }
+                else if(queue[firstChild] > queue[secondChild]){
+                    queue[parent] = queue[firstChild];
+                    queue[firstChild] = temp;
+                    
+                    this.trickleDown(queue,firstChild,end -1);
+                }
+                else if(queue[firstChild] <= queue[secondChild]){
+                    queue[parent] = queue[secondChild];
+                    queue[secondChild] = temp;
+                    this.trickleDown(queue,secondChild,end -1);
+                
+                }
+
+
+
+            }
+            else{
+                if(queue[firstChild] > queue[secondChild]){
+                    queue[parent] = queue[firstChild];
+                    queue[firstChild] = temp;
+                    
+                    this.trickleDown(queue,firstChild);
+                }
+                else if(queue[firstChild] <= queue[secondChild]){
+                    queue[parent] = queue[secondChild];
+                    queue[secondChild] = temp;
+                    this.trickleDown(queue,secondChild);
+                
+                }
+            }
+        
+        //if our first child is greater than our second, swap parent with first
+            
+        }
+
+    }
+
+    extractMax(){
+        let first = 0;
+        let last = this.queue.length -1;
+
+        let extracted = this.queue[first];
+
+        this.queue[first] = this.queue[last];
+        this.queue.pop();
+
+        this.trickleDown(this.queue,first);
+
+        return extracted;
+    }
+
+    maxHeapify(){
+        let length = this.queue.length;
+        
+        for(let i = length -1; i >= 0; i--){
+         
+            this.trickleDown(this.queue,this.calcParent(i));
+
+        }
+
+        
+
+    }
+    
+            
+    heapSort(){
+        
+        for(let i = this.queue.length -1; i > 0 ;i -- ){
+
+            let first = 0;
+            let last = i;
+            let temp = this.queue[first];
+            this.queue[first] = this.queue[last];
+            this.queue[last] = this.queue[temp];
+
+            this.trickleDown(this.queue,this.calcParent(i),i);
+            
+          
+        }
+
+
+    }
 
 
 
@@ -61,6 +179,27 @@ myHeap.insert(6);
 myHeap.insert(5);
 myHeap.insert(3);
 myHeap.insert(1);
+myHeap.insert(0);
+myHeap.insert(4);
+myHeap.insert(8);
+myHeap.insert(2);
+myHeap.insert(5);
 console.log(myHeap.checkQueue());
+// myHeap.heapSort();
+let max = myHeap.extractMax();
+console.log(max);
+console.log(myHeap);
+let secmax = myHeap.extractMax();
+console.log(secmax);
+console.log(myHeap);
 
+let newQueue = [1,4,2,5,6,7,4,9,5]
+let newHeap = new minHeap();
+newHeap.queue = newQueue;
+
+console.log(newHeap.queue)
+newHeap.maxHeapify();
+console.log(newHeap.queue)
+// newHeap.heapSort();
+// console.log('sorted',newHeap.queue)
 //do binary tree keys in order next
