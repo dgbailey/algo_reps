f137bf1fdcab68d123b91c14277dad094933847e
dgbailey <bailey.dust@gmail.com>
2019-11-14 14:21:43 -0800

    more reps

---
+++ systemArch.js
@@ -0,0 +1,12 @@
+/*
+
+
+1) Scope the problem OR design for a smaller scale at first
+2) Clarify or establish assumptions
+3) Propose potential solitions
+4) Identify potential bottleknecks
+
+
+
+
+*/
