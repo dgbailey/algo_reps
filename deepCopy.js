05aac4c15654c9907de6ab99863b12e4f94781d4
dgbailey <bailey.dust@gmail.com>
2019-11-22 10:14:06 -0800

    more reps

--- deepCopy.js
+++ deepCopy.js
@@ -1,23 +1,79 @@
-function deepHelper(arr,newArr=[]){
-    //takes an array
-    //recursively builds new array of objects
+//you weren't associating existing vertex values in ref with new neighbor copies
+//dfs doesn't always require a stack
+
+var cloneGraph = function(node) {
+    if (!node) return node;
     
-    console.log(arr)
-   
-    for(let i = 0;i < arr.length; i++){
-        
-        newArr.push(new Node(arr[i].val,deepHelper(arr[i].neighbors)))
-        console.log(arr);
+    const ref = {};
+    const copy = new Node(node.val, []);
+    
+    ref[node.val] = copy;
+    dfs(node, ref);
+    return copy;
+};
+
+var dfs = function(node, ref) {
+    for (neighbor of node.neighbors) {
+        if (!ref[neighbor.val]) {
+            const copy = new Node(neighbor.val, []);
+            ref[neighbor.val] = copy;
+            ref[node.val].neighbors.push(copy);
+            dfs(neighbor, ref)
+        } else {
+            ref[node.val].neighbors.push(ref[neighbor.val])
+        }
+    }
+}
+
+/**
+ * // Definition for a Node.
+ * function Node(val,neighbors) {
+ *    this.val = val;
+ *    this.neighbors = neighbors;
+ * };
+ */
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    
+    if(!node){
+        return node;
     }
     
-    return newArr;
-}
+    let refCopies = {};
+    let copy = new Node(node.val,[]);
+    refCopies[copy.val] = copy;
+    deepHelper(node,refCopies);
+    return copy;
+    
 
-function Node(val,neighbors) {
-    this.val = val;
-    this.neighbors = neighbors;
-}
-
-let test = [{"$id":"2","neighbors":[{"$ref":"1"},{"$id":"3","neighbors":[{"$ref":"2"},{"$id":"4","neighbors":[{"$ref":"3"},{"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}];
-
-console.log(deepHelper(test));
+    
+    function deepHelper(node,reference){
+        for(let n of node.neighbors){
+            if(!reference[n.val]){
+            
+                reference[n.val] = new Node(n.val,[]);
+            //add to ref
+            //create copy for neighbors
+            
+                
+                deepHelper(n,reference);
+            }
+        
+            //call deepHelp on new neighbor
+            
+        
+        else{
+            //add reference to current neighbors array
+            reference[node.val].neighbors.push(reference[n.val]);
+        }
+        }
+        
+    }
+        
+       
+    
+    
+};
