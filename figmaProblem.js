fd3421f14801bcb83a3d458eb18eb5bd58e96c4b
dgbailey <bailey.dust@gmail.com>
2019-11-25 17:33:15 -0800

    more reps

---
+++ figmaProblem.js
@@ -0,0 +1,23 @@
+//given a query and a text string 
+//insert appropriate highlights
+
+//(fig, 'fgconfigurefigma')
+
+const highlight(query,text){
+    let response = '';
+    //store matched characters in array
+    //[0,5,11,6,12,1,7,13,]
+    //sorted 0,1,5,6,7,11,12,13
+    //fg, fig, fig
+
+    //BST?
+    //BT?
+    //return <b> surrounding highlighted characters
+
+    
+
+
+
+
+    return response;
+}
